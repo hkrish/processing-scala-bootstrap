81416b992d9e02575c0ddce93fbfbb19bfd3cd28
hkrish <hari.exeption@gmail.com>
2013-09-09 20:46:26 +0200

    Add a run script

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-cd target/scala-2.10/classes && scala -cp ./:../../../lib/core.jar:../../../lib/gluegen-rt.jar:../../../lib/jogl-all.jar:../../../lib/psmoveapi.jar -Djava.library.path=../../../lib PBootstrap
+cd target/scala-2.10/classes && scala -cp ./:../../../lib/core.jar:../../../lib/gluegen-rt.jar:../../../lib/jogl-all.jar -Djava.library.path=../../../lib PBootstrap
